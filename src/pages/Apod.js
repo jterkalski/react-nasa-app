e2e6b5aa0405168e80fe007a14feb120e2944b34
Jakub <jakub.terkalski@gmail.com>
2022-10-03 10:50:30 +0200

    added apod fetching

--- src/pages/Apod.js
+++ src/pages/Apod.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// Astronomy picture of the day
 
 const Apod = () => {
-    return <div>Apod</div>;
+    const [loading, setLoading] = useState(false);
+    const [apodData, setApodData] = useState();
+
+    // NASA api key generated on: https://api.nasa.gov/
+    const apiKey = 'wq5ELeUZXIYmD9e0Ad8NsY5XACyEnkwe3JdsuyF6';
+
+    useEffect(() => {
+        const fetchApod = async () => {
+            try {
+                setLoading(true);
+                const res = await axios.get('https://api.nasa.gov/planetary/apod', {
+                    params: {
+                        api_key: apiKey,
+                    },
+                });
+                setApodData(res.data);
+                setLoading(false);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        fetchApod();
+    }, []);
+
+    return (
+        <div>
+            {loading ? <div>Loading...</div> : <div>{JSON.stringify(apodData)}</div>}
+        </div>
+    );
 };
 
 export default Apod;
